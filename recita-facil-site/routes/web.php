24cdc2e00c47146323ba96cbc9a88238e78a39c6
Douglas Alves <douglasreisalves02@gmail.com>
2022-11-19 14:29:03 -0300

    rota pagina inicial e configuração inicial do db

--- recita-facil-site/routes/web.php
+++ recita-facil-site/routes/web.php
@@ -1,25 +1,22 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+
+use App\Http\Controllers\ReceitasController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get ('/', function(){
+    return view('index');
+})->name('page.index');
+
+route::get('/receitas',[ReceitasController::class, 'index'])->name('receitas.index');
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
